9a0d896b1922fe408a3347cfa5eb80f42116ed08
Michael Häusler <michael.haeusler@researchgate.net>
2018-05-25 03:00:50 +0200

    display resonators in portal view

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,14 @@ class App extends Component {
         portal: {
             controllingFaction: neutral
         },
+        health: 0,
         capture: false,
         changeMillis: 4242
-    }
+    };
 
     componentWillMount() {
         this.updatePortalData();
-        this.timer = setInterval(() => this.updatePortalData(), 200);
+        this.timer = setInterval(() => this.updatePortalData(), 20);
     }
 
     updatePortalData() {
@@ -32,6 +33,12 @@ class App extends Component {
                     var portal = data.result;
                     if (!portal.controllingFaction) {
                         portal.controllingFaction = neutral;
+                    }
+                    var health = 0.0;
+                    if (portal.resonators) {
+                        health = portal.resonators.reduce((acc, resonator) => {
+                            acc + resonator.health
+                        }, 0.0) / portal.resonators.length;
                     }
                     var change = portal.controllingFaction !== prevState.portal.controllingFaction;
                     var capture = change && portal.controllingFaction !== neutral;
@@ -50,22 +57,37 @@ class App extends Component {
                         document.body.classList.toggle('res', portal.controllingFaction === resistance);
                         document.body.classList.toggle('enl', portal.controllingFaction === enlightened);
                     }
-
                     var nextState = {
                         portal: data.result,
+
                         capture: capture,
                         changeMillis: changeMillis
                     };
                     return nextState;
                 });
-            });
-    }
-
-    componentDidMount() {
-
+            })
+            .catch(error => console.error(error));
     }
 
     render() {
+        let resonatorOwners;
+        if (this.state.portal.resonators) {
+            resonatorOwners = this.state.portal.resonators.map(resonator => {
+                return (
+                    <span key={'ro' + resonator.position} className={resonator.position}>{resonator.owner || ''}</span>
+                );
+            });
+        }
+
+        let resonatorLevels;
+        if (this.state.portal.resonators) {
+            resonatorLevels = this.state.portal.resonators.map(resonator => {
+                return (
+                    <div key={'rl' + resonator.position} className={'rl' + resonator.position + ' l' + resonator.level}>{resonator.level}</div>
+                );
+            });
+        }
+
         return (
             <div className="main">
                 <div className="faction"><span className="res">{this.state.portal.controllingFaction}</span></div>
@@ -73,27 +95,13 @@ class App extends Component {
                 <div className="portallevel"><span>{this.state.portal.level}</span></div>
                 <div className="playername"><span>{this.state.portal.owner}</span></div>
                 <div className="resoplayer">
-                    <span className="one">akatose</span>
-                    <span className="two">BlackDevils27</span>
-                    <span className="three">Fantasio</span>
-                    <span className="four">Zorig</span>
-                    <span className="five">Tropus</span>
-                    <span className="six">DrayPrescot</span>
-                    <span className="seven">vitus365</span>
-                    <span className="eight">genmaicha</span>
+                    {resonatorOwners}
                 </div>
                 <div className="resos">
-                    <div className="l1">1</div>
-                    <div className="l2">2</div>
-                    <div className="l3">3</div>
-                    <div className="l4">4</div>
-                    <div className="l5">5</div>
-                    <div className="l6">6</div>
-                    <div className="l7">5</div>
-                    <div className="l8">8</div>
+                    {resonatorLevels}
                 </div>
                 <div className="portalname"><span>{this.state.portal.title}</span></div>
-                <div className="energy"><span>48k</span></div>
+                <div className="energy"><span>{this.state.health}</span></div>
                 <div className="mods">
                     <div className="mod1"><span className="modtext">CMH</span></div>
                     <div className="mod2"><span className="modtext">RHS</span></div>
@@ -101,7 +109,7 @@ class App extends Component {
                     <div className="mod4"><span className="modtext">CS</span></div>
                 </div>
                 <div className="slots">
-                    <div className="comm"><span className="commtext">E8 / NE7 / N7 / NW7 / W6 / SW6 / S5 / SE8</span></div>
+                    <div className="comm"><span className="commtext">N / NE / E / SE / S / SW / W / NW</span></div>
                 </div>
                 <Video triggerOpen={this.state.capture}/>
             </div>
--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -2,16 +2,22 @@ import React from 'react';
 import './Video.css';
 
 class Video extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false
-        };
+    state = {
+        open: false,
+        currentVideo: -1,
+        currentVideoIndex: null,
+        maxVideo: 28
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
         if (nextProps.triggerOpen && !prevState.open) {
-            return { open: true };
+            var currentVideo = (prevState.currentVideo + 1) % (prevState.maxVideo + 1);
+            return {
+                open: true && false,
+                currentVideo: currentVideo,
+                currentVideoIndex: String('0' + currentVideo).slice(-2),
+                maxVideo: prevState.maxVideo
+            };
         }
         return null;
     }
@@ -23,13 +29,14 @@ class Video extends React.Component {
     render() {
         console.log({
             triggerOpen: this.props.triggerOpen,
-            videoOpen: this.state.open
+            videoOpen: this.state.open,
+            currentVideoIndex: this.state.currentVideoIndex
         });
         if (!this.state.open) {
             return null;
         }
         return (
-            <video ref="portalMedia" className="video" src="/video/001.webm" type="video/webm" autoPlay="true"
+            <video ref="portalMedia" className="video" src={"/video/loop/" + this.state.currentVideoIndex + ".mp4"} type="video/mp4" autoPlay="true"
                    onEnded={this.handleVideoStop.bind(this)}
                    onAbort={this.handleVideoStop.bind(this)}
                    onEmptied={this.handleVideoStop.bind(this)}
